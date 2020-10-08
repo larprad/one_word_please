e85d87d4623681a9006d02c6812f09662688654d
larprad <59915248+larprad@users.noreply.github.com>
2020-10-08 10:13:00 +0200

    Include Main folder in repo

---
+++ build/precache-manifest.b3e29a0c038e4fcc155e118ecb754b8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f4b1e9a86969f52cd1295a70e0ba9ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "025ade4e40830f8b6820",
+    "url": "/static/css/main.25c4c05a.chunk.css"
+  },
+  {
+    "revision": "2cdca7c2d7660aceae1f",
+    "url": "/static/js/2.6d997d4b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.6d997d4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "025ade4e40830f8b6820",
+    "url": "/static/js/main.7a49a17e.chunk.js"
+  },
+  {
+    "revision": "90902986ec86c0403997",
+    "url": "/static/js/runtime-main.8e288c19.js"
+  }
+]);
