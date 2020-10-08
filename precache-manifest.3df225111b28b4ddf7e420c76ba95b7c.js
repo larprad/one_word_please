eb17a9f596bc6be898f06bb6474ed550d98a54ea
larprad <59915248+larprad@users.noreply.github.com>
2020-10-08 10:58:57 +0200

    Updates

---
+++ precache-manifest.3df225111b28b4ddf7e420c76ba95b7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e6177fef87a75b3ab2ed572a5656e3",
+    "url": "/one_word_please/index.html"
+  },
+  {
+    "revision": "eb90f40e623e7a513f2e",
+    "url": "/one_word_please/static/css/main.11d96fea.chunk.css"
+  },
+  {
+    "revision": "7d252aa7cfb2b317b6f5",
+    "url": "/one_word_please/static/js/2.e4742e85.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/one_word_please/static/js/2.e4742e85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb90f40e623e7a513f2e",
+    "url": "/one_word_please/static/js/main.9b2e5f71.chunk.js"
+  },
+  {
+    "revision": "3af48049cc7943e717ec",
+    "url": "/one_word_please/static/js/runtime-main.d7d8fe3f.js"
+  },
+  {
+    "revision": "02bd0963fb9430865fb7cb42bfd44279",
+    "url": "/one_word_please/static/media/oops.02bd0963.png"
+  }
+]);
